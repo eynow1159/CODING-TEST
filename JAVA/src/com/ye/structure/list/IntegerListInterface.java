5521e8c1939c62333a419cc8faa774c65669c4ed
eynow <eynow1159@gmail.com>
2026-01-29 16:01:54 +0900

    IntegerListInterface

---
+++ JAVA/src/com/ye/structure/list/IntegerListInterface.java
@@ -0,0 +1,14 @@
+package com.ye.structure.list;
+
+public interface IntegerListInterface {
+	public void add(int index, Integer x);
+	public void append(Integer x);
+	public Integer remove(int index);
+	public boolean removeItem(Integer x);
+	public Integer get(int index);
+	public void set(int index, Integer x);
+	public int indexOf(Integer x);
+	public int len();
+	public boolean isEmpty();
+	public void clear();
+}

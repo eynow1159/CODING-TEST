a7eed3e20498db1afe73b99d01c82f83e73b8690
eynow <eynow1159@gmail.com>
2026-02-11 19:27:31 +0900

    structure.IndexInterface<R, E>

--- JAVA/src/com/ye/structure/IndexInterface.java
+++ JAVA/src/com/ye/structure/IndexInterface.java
@@ -1,13 +1,13 @@
 package com.ye.structure;
 
-public interface IndexInterface<T, K> {
+public interface IndexInterface<R, E> {
 //public interface IndexInterface<T> {
 //	public T search(Comparable x);
 //	public void insert(Comparable x);
 //	public void delete(Comparable x);
-	public T search(K x);
-	public void insert(K x);
-	public void delete(K x);
+	public R search(E x);
+	public void insert(E x);
+	public void delete(E x);
 	public boolean isEmpty();
 	public void clear();
 }
